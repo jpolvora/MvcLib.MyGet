024ae3e9530f87a248fcedccea28d0f1816f92d6
jpolvora <jpolvora@gmail.com>
2014-09-03 10:32:09 -0400

    readd tree

--- MvcLib/MvcLib.DbFileSystem/DbFileContextMigrationInitializer.cs
+++
@@ -1,9 +0,0 @@
-using System.Data.Entity;
-
-namespace MvcLib.DbFileSystem
-{
-    public class DbFileContextMigrationInitializer : MigrateDatabaseToLatestVersion<DbFileContext, DbFileContextMigrationConfiguration>
-    {
-        
-    }
-}
